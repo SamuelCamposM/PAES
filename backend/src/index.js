beba1dccd7cd67ed981ee2c5f25863563fce0903
SamuelCamposM <samuelcampos720@outlook.com>
2020-07-25 12:19:07 -0600

    deleting images

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,38 +1,37 @@
 import express from "express";
-import cors from "cors"
-import mongoose from "mongoose"
-import multer from "multer"
-import path from "path"
-mongoose.set('useCreateIndex', true);
+import cors from "cors";
+import mongoose from "mongoose";
+import multer from "multer";
+import path from "path";
+mongoose.set("useCreateIndex", true);
 //conectar a la base de datos
 import "./config/database";
-import passport from "passport"
-import morgan from "morgan"
+import passport from "passport";
+import morgan from "morgan";
 const app = express();
 //middlewares
-app.use(cors())
-app.use(passport.initialize())
-app.use(morgan('dev'))
-app.use(express.json({extended : true }))
+app.use(cors());
+app.use(passport.initialize());
+app.use(morgan("dev"));
+app.use(express.json({ extended: true }));
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname , "./public/images"),
-    filename: function(req, file, cb){
-       cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
-    }
- });
-
+  destination: path.join(__dirname, "./public/images"),
+  filename: function (req, file, cb) {
+    cb(null, "IMAGE-" + Date.now() + path.extname(file.originalname));
+  },
+});
 
 export const upload = multer({
-   storage: storage,
-   limits:{fileSize: 1000000},
+  storage: storage,
+  limits: { fileSize: 1000000 },
 }).single("myImage");
 //routes
-app.use('/usuarios', require('./routes/usuarios'))
-app.use('/', require('./routes/google'))
-app.use('/imagen', require('./routes/imagen'))
+app.use("/usuarios", require("./routes/usuarios"));
+app.use("/", require("./routes/google"));
+app.use("/imagen", require("./routes/imagen"));
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 let port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`server on port ${port}`));
--- backend/src/routes/imagen.js
+++ backend/src/routes/imagen.js
@@ -1,27 +1,41 @@
 import { Router } from "express";
 import { upload } from "../index";
 import Usuario from "../models/Usuario";
+import { unlinkSync } from "fs";
+
 import token from "../middlewares/token";
-import path from "path"
+import path from "path";
 const router = Router();
 
 // file upload api
-router.post("/upload", token ,  (req, res) => {
-  
+router.post("/upload", token, (req, res) => {
   upload(req, res, async (err) => {
     console.log("Request file ---", req.file); //Here you get file.
-    const _id = req.usuario.id
+    const _id = req.usuario.id;
     /*Now do where ever you want to do*/
 
     if (!err) {
-        const usuario = await Usuario.findOneAndUpdate({_id} , {avatar : `http://localhost:4000/imagen/image/${req.file.filename}`  } ,{new : true} )
-      return res.status(200).json({ datos:usuario.avatar  });
-      
+      const { avatar } = await Usuario.findOne({ _id });
+      console.log(avatar);
+      if (avatar) {
+        const imagen = avatar.split("/");
+        console.log(imagen[5]);
+        await unlinkSync(
+          path.join(`${__dirname}/../public/images/${imagen[5]}`)
+        );
+        console.log(path.resolve(`/src/public/images/${imagen[5]}`));
+      }
+
+      const usuario = await Usuario.findOneAndUpdate(
+        { _id },
+        { avatar: `http://localhost:4000/imagen/image/${req.file.filename}` },
+        { new: true }
+      );
+      return res.status(200).json({ datos: usuario.avatar });
     }
   });
 });
-router.get("/image/:id", (req , res) => {
-    res.sendFile(path.join(__dirname,`../public/images/${req.params.id}`))
-    
-})
+router.get("/image/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, `../public/images/${req.params.id}`));
+});
 module.exports = router;
